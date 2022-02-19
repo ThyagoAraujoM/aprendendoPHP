b7a82f70ac87a893f7441a600630a2b1023e82d0
Thyago <thyagoaraujomotta@gmail.com>
2022-02-19 19:54:27 -0300

    Adicionando data e items aos eventos assim como sistema de busca

--- primeiroProjetoLaravel/app/Http/Controllers/EventController.php
+++ primeiroProjetoLaravel/app/Http/Controllers/EventController.php
@@ -11,10 +11,16 @@ class EventController extends Controller
 
     public function index()
     {
+        $search = request("search");
+        if ($search) {
+            $events = Event::where([
+                ["title", "like", "%" . $search . "%"]
+            ])->get();
+        } else {
+            $events = Event::all();
+        }
 
-        $events = Event::all();
-
-        return view('welcome', ['events' => $events]);
+        return view('welcome', ['events' => $events, 'search' => $search]);
     }
 
     public function create()
@@ -31,6 +37,10 @@ class EventController extends Controller
         $event->city = $request->city;
         $event->private = $request->private;
         $event->description = $request->description;
+        $event->date = $request->date;
+        $event->items = $request->items;
+
+        // Image Upload
         if ($request->hasFile('image') && $request->file("image")->isValid()) {
             $requestImage = $request->image;
             $extension = $requestImage->extension();
@@ -41,5 +51,12 @@ class EventController extends Controller
         $event->save();
 
         return redirect('/')->with("msg", "Evento criado com sucesso");
+    }
+
+    public function show($id)
+    {
+        $event = Event::findOrFail($id);
+
+        return view("events.show", ['event' => $event]);
     }
 }
--- primeiroProjetoLaravel/app/Models/Event.php
+++ primeiroProjetoLaravel/app/Models/Event.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Event extends Model
 {
     use HasFactory;
+
+    // Para dizer que o tipo de items é um array e não uma string
+    // Uma das poucas alterações que precisam ser feitas
+    // no models para o entendimento de alguns arquivos
+    protected $casts = [
+        'items' => 'array'
+    ];
+
+    protected $dates = ['date'];
 }
